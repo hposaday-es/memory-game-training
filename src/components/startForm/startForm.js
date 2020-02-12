976f104d80ef93fe2c5607cffe0e32a3ff1ae90c
esinergia <hector.posada@ecosingergia.co>
2020-02-12 16:58:18 -0500

    startform add

---
+++ src/components/startForm/startForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+
+class StartForm extends React.Component { 
+
+    render () {
+        return (
+            <Container>
+                
+            </Container>
+        )
+    }
+}
